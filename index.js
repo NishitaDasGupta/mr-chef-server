4581d168ac6eae3a593d70ec1f8f252a1dc76674
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-05-03 16:59:58 +0600

    chef details API done

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chefDetails = require('./data/chefDetails.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chefs', (req, res) => {
+  res.send(chefDetails);
+})
+
+app.get('/chefs/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedChef = chefDetails.find( chef => chef.Chef_ID === id);
+  res.send(selectedChef);
 })
 
 app.listen(port, () => {
